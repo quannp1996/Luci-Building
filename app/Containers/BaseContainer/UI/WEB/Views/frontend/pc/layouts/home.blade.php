3a3adba011aad5867198d09048dc5eab1bce869f
Quan NP <quannguyenphu@vccorp.vn>
2022-06-25 09:29:09 +0700

    loại bỏ code không cần thiết

--- app/Containers/BaseContainer/UI/WEB/Views/frontend/pc/layouts/home.blade.php
+++ app/Containers/BaseContainer/UI/WEB/Views/frontend/pc/layouts/home.blade.php
@@ -159,55 +159,6 @@
         id="jquery-core-js"></script>
     <script type="text/javascript" src="{{ asset('js/sweetalert2.all.min.js') }}"
         id="jquery-core-js"></script>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-170799243-1"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag("js", new Date());
-
-        gtag("config", "UA-170799243-1");
-    </script>
-    {{-- <script type="text/javascript">
-        (function(m, e, t, r, i, k, a) {
-            m[i] =
-                m[i] ||
-                function() {
-                    (m[i].a = m[i].a || []).push(arguments);
-                };
-            m[i].l = 1 * new Date();
-            (k = e.createElement(t)), (a = e.getElementsByTagName(t)[0]), (k.async = 1), (k.src = r), a.parentNode
-                .insertBefore(k, a);
-        })(window, document, "script", "../../../mc.yandex.ru/metrika/tag.js", "ym");
-
-        ym(69065527, "init", {
-            clickmap: true,
-            trackLinks: true,
-            accurateTrackBounce: true,
-            webvisor: true,
-            ecommerce: "dataLayer",
-        });
-    </script> --}}
-    <noscript>
-        <div><img src="https://mc.yandex.ru/watch/69065527" style="position: absolute; left: -9999px;"
-                alt="" /></div>
-    </noscript>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-604199103"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag("js", new Date());
-
-        gtag("config", "AW-604199103");
-    </script>
-    <meta name="p:domain_verify" content="faaa21a6850d62c5f6b71baf45d4dda1" />
-    <link rel="alternate" href="index.html" hreflang="vi" />
-    <link rel="alternate" href="https://luci.vn/en/product/luci-building-en/" hreflang="en" />
     <style>
         .bg {
             opacity: 0;
@@ -219,36 +170,6 @@
             opacity: 1;
         }
     </style>
-    {{-- <script>
-        !(function(f, b, e, v, n, t, s) {
-            if (f.fbq) return;
-            n = f.fbq = function() {
-                n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
-            };
-            if (!f._fbq) f._fbq = n;
-            n.push = n;
-            n.loaded = !0;
-            n.version = "2.0";
-            n.queue = [];
-            t = b.createElement(e);
-            t.async = !0;
-            t.src = v;
-            s = b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t, s);
-        })(window, document, "script", "../../../connect.facebook.net/en_US/fbevents.js");
-        fbq("init", "2181255115516857");
-        fbq("track", "PageView");
-    </script> --}}
-    
-    <noscript><img height="1" width="1" style="display: none;"
-            src="https://www.facebook.com/tr?id=2181255115516857&amp;ev=PageView&amp;noscript=1" /></noscript>
-    <noscript>
-        <style>
-            .woocommerce-product-gallery {
-                opacity: 1 !important;
-            }
-        </style>
-    </noscript>
 </head>
 
 <body class="home">
